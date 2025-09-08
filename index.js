e9bf001682146ead3107008951bdc5fb4552d703
Vasiliy Movchan <movchan.vasia@gmail.com>
2025-09-08 20:34:32 +0300

    виправлення шафлу

--- index.js
+++ index.js
@@ -51,132 +51,82 @@ for (let i = 1; i <= 20; i++) {
 function shuffle(array) {
     let arr = array.slice();
 
-    // Вилучаємо учнів 15, 7, 13, 12, 5, 8, 17
-    const idx15 = arr.findIndex((name) => name === students[15]);
-    const student15 = arr.splice(idx15, 1)[0];
+    // Вилучаємо учнів з особливими обмеженнями
+    const special = [
+        { num: 15, name: students[15] },
+        { num: 7, name: students[7] },
+        { num: 13, name: students[13] },
+        { num: 12, name: students[12] },
+        { num: 5, name: students[5] },
+        { num: 8, name: students[8] },
+        { num: 17, name: students[17] },
+        { num: 14, name: students[14] },
+        { num: 16, name: students[16] },
+    ];
+    special.forEach((s) => {
+        const idx = arr.findIndex((name) => name === s.name);
+        arr.splice(idx, 1);
+    });
 
-    const idx7 = arr.findIndex((name) => name === students[7]);
-    const student7 = arr.splice(idx7, 1)[0];
-
-    const idx13 = arr.findIndex((name) => name === students[13]);
-    const student13 = arr.splice(idx13, 1)[0];
-
-    const idx12 = arr.findIndex((name) => name === students[12]);
-    const student12 = arr.splice(idx12, 1)[0];
-
-    const idx5 = arr.findIndex((name) => name === students[5]);
-    const student5 = arr.splice(idx5, 1)[0];
-
-    const idx8 = arr.findIndex((name) => name === students[8]);
-    const student8 = arr.splice(idx8, 1)[0];
-
-    const idx17 = arr.findIndex((name) => name === students[17]);
-    const student17 = arr.splice(idx17, 1)[0];
-
-    // Перемішуємо масив без учнів 15, 7, 13, 12, 5, 8, 17
+    // Перемішування масиву без особливих учнів
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 
-    // Визначаємо індекси спарених парт (2-3, 4-5, ..., 17-18)
-    const pairedIndexes = [];
-    for (let i = 1; i <= 18; i += 2) {
-        pairedIndexes.push([i - 1, i]);
-    }
-
-    let pos7,
-        pos13,
-        pos12,
-        pos5,
-        pos8,
-        pos15,
-        pos17,
-        valid = false;
+    let valid = false,
+        tempArr;
     while (!valid) {
-        pos7 = Math.floor(Math.random() * (arr.length + 1));
-        pos13 = Math.floor(Math.random() * (arr.length + 2));
-        pos12 = Math.floor(Math.random() * (arr.length + 3));
-        pos5 = Math.floor(Math.random() * (arr.length + 4));
-        pos8 = Math.floor(Math.random() * (arr.length + 5));
-        pos15 = Math.floor(Math.random() * (arr.length + 6));
-        pos17 = Math.floor(Math.random() * (arr.length + 7));
-        // Всі позиції мають бути різними
-        if (new Set([pos7, pos13, pos12, pos5, pos8, pos15, pos17]).size < 7) continue;
+        // Випадкові позиції для особливих учнів
+        let positions = [];
+        let arrCopy = arr.slice();
+        for (let i = 0; i < special.length; i++) {
+            let pos;
+            do {
+                pos = Math.floor(Math.random() * (arrCopy.length + 1));
+            } while (positions.includes(pos));
+            positions.push(pos);
+            arrCopy.splice(pos, 0, special[i].name);
+        }
+        tempArr = arrCopy;
 
-        // Вставляємо тимчасово
-        let tempArr = arr.slice();
-        tempArr.splice(pos7, 0, student7);
-        let adjPos13 = pos13 > pos7 ? pos13 : pos13;
-        tempArr.splice(adjPos13, 0, student13);
-        let adjPos12 = pos12;
-        if (adjPos12 > pos7) adjPos12++;
-        if (adjPos12 > adjPos13) adjPos12++;
-        tempArr.splice(adjPos12, 0, student12);
-
-        let adjPos5 = pos5;
-        if (adjPos5 > pos7) adjPos5++;
-        if (adjPos5 > adjPos13) adjPos5++;
-        if (adjPos5 > adjPos12) adjPos5++;
-        tempArr.splice(adjPos5, 0, student5);
-
-        let adjPos8 = pos8;
-        if (adjPos8 > pos7) adjPos8++;
-        if (adjPos8 > adjPos13) adjPos8++;
-        if (adjPos8 > adjPos12) adjPos8++;
-        if (adjPos8 > adjPos5) adjPos8++;
-        tempArr.splice(adjPos8, 0, student8);
-
-        let adjPos15 = pos15;
-        if (adjPos15 > pos7) adjPos15++;
-        if (adjPos15 > adjPos13) adjPos15++;
-        if (adjPos15 > adjPos12) adjPos15++;
-        if (adjPos15 > adjPos5) adjPos15++;
-        if (adjPos15 > adjPos8) adjPos15++;
-        tempArr.splice(adjPos15, 0, student15);
-
-        let adjPos17 = pos17;
-        if (adjPos17 > pos7) adjPos17++;
-        if (adjPos17 > adjPos13) adjPos17++;
-        if (adjPos17 > adjPos12) adjPos17++;
-        if (adjPos17 > adjPos5) adjPos17++;
-        if (adjPos17 > adjPos8) adjPos17++;
-        if (adjPos17 > adjPos15) adjPos17++;
-        tempArr.splice(adjPos17, 0, student17);
-
-        // Перевіряємо умови
+        // Перевірка обмежень
         valid = true;
-        for (let [a, b] of pairedIndexes) {
-            // 7 і 13 не поруч
-            if ((tempArr[a] === students[7] && tempArr[b] === students[13]) || (tempArr[a] === students[13] && tempArr[b] === students[7])) {
+        // 7 і 13 не поруч на спарених партах
+        // 12 і 15 не поруч на спарених партах
+        // 5 та 8 не можуть бути на партах > 13 (індекс > 13)
+        // 15 не може бути на партах > 13 (індекс > 13)
+        // 17 не може бути на партах > 15 (індекс > 14)
+        // 14 та 16 не можуть бути на партах < 10 (індекс < 9)
+        for (let i = 1; i <= 18; i += 2) {
+            // 7 і 13
+            if ((tempArr[i - 1] === students[7] && tempArr[i] === students[13]) || (tempArr[i - 1] === students[13] && tempArr[i] === students[7])) {
                 valid = false;
                 break;
             }
-            // 12 і 15 не поруч
-            if ((tempArr[a] === students[12] && tempArr[b] === students[15]) || (tempArr[a] === students[15] && tempArr[b] === students[12])) {
+            // 12 і 15
+            if (
+                (tempArr[i - 1] === students[12] && tempArr[i] === students[15]) ||
+                (tempArr[i - 1] === students[15] && tempArr[i] === students[12])
+            ) {
                 valid = false;
                 break;
             }
         }
-        // 5 та 8 не можуть бути на партах з номером більше 13 (індекси 13-19)
-        const idx5final = tempArr.indexOf(student5);
-        const idx8final = tempArr.indexOf(student8);
-        if (idx5final > 13 || idx8final > 13) valid = false;
-
-        // 15 не може бути на партах з номером більше 13 (індекси 13-19)
-        const idx15final = tempArr.indexOf(student15);
-        if (idx15final > 13) valid = false;
-
-        // 17 не може бути на партах з номером більше 15 (індекси 15-19)
-        const idx17final = tempArr.indexOf(student17);
-        if (idx17final > 14) valid = false;
-
-        if (valid) {
-            arr = tempArr;
-        }
+        const idx5 = tempArr.indexOf(students[5]);
+        const idx8 = tempArr.indexOf(students[8]);
+        const idx15 = tempArr.indexOf(students[15]);
+        const idx17 = tempArr.indexOf(students[17]);
+        const idx14 = tempArr.indexOf(students[14]);
+        const idx16 = tempArr.indexOf(students[16]);
+        if (idx5 > 12 || idx8 > 12) valid = false;
+        if (idx15 > 12) valid = false;
+        if (idx17 > 14) valid = false;
+        if (idx14 < 8 || idx16 < 8) valid = false;
+        console.log('Ітерація');
     }
 
-    return arr;
+    return tempArr;
 }
 
 const placeBtn = document.querySelector('.place-btn');
